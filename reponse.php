372ce3979e9b32e65fdda54fef4e66aa17626df6
Lucie KHAMLACH <lkhamlac@e2r12p2.clusters.42paris.fr>
2022-12-30 15:32:25 +0100

    MERGE

--- reponse.php
+++ reponse.php
@@ -1,8 +1,52 @@
 <!DOCTYPE html>
-<html>
+<title >webserv's super html page</title>
+
+<body id =\"all\" style="font-family:'Roboto',sans-serif;margin: 0em; padding: 0em;">
+<div class="header" style="display:block; background-color: orange; text-align: center; padding: 15px; "><h1 style="
+font-weight: bold;
+font-size: 30px;
+margin-top: 0px;
+margin-bottom: 10px;font-family:'Roboto',sans-serif;  "><a href="/html/home.html" style="text-decoration: none; color: white;">webserv index</a></h1>
+</div>
+
+<div class="menu" >
+<ul style=" margin: 0em; padding: 0em; list-style-type: none; overflow: hidden; background-color: #333;">
+    <li style="float:left;"><a href="/html/home.html" style="display: block; color: white; text-align: center; padding: 14px 16px; text-decoration: none;">accueil</a></li>
+    <li style="float:left;"><a href="/html/galerie.html" style="display: block; color: white; text-align: center; padding: 14px 16px; text-decoration: none;">galerie</a></li>
+    <li style="float:left;"><a href="/html/text.php" style="display: block; color: white; text-align: center; padding: 14px 16px; text-decoration: none;">upload text</a></li>
+</ul>
+</div>
+
+<h2 style="font-family:'Roboto',sans-serif;text-align: center;"> ca fonctionne c tiptoptap</h2>
+
+<hr>
 	
-<body>
-	Bonjour, <?php echo htmlspecialchars($_GET['fname']); ?>.
-	Tu as <?php echo $_GET['lname']; ?> ans.
+
+Bonjour, <?php echo htmlspecialchars($_GET['fname']); ?>.
+Tu as <?php echo $_GET['lname']; ?> ans.
+
+
+<hr>
+<footer style="background-color: orange;
+position: absolute;
+left: 0;
+bottom: 0;
+height: 20%;
+width: 100%;
+overflow: hidden;">
+    <h2>The team</h2>
+    <p>
+        <strong>Lucie Khamlach:</strong>
+        <a href="https://profile.intra.42.fr/users/lkhamlac"> Intra Profile</a>
+    </p>
+    <p>
+        <strong>Julien Taravella:</strong>
+        <a href="https://profile.intra.42.fr/users/jtaravel"> Intra Profile</a>
+    </p>
+    <p>
+        <strong>Mehdi Mhaya:</strong>
+        <a href="https://profile.intra.42.fr/users/mmhaya"> Intra Profile</a>
+    </p>
+  </footer>
 </body>
 </html>
